dca1b7ecf6d027ccd9ea7102a45fb9cda0f56e21
asalazara1 <52968551+asalazara1@users.noreply.github.com>
2019-07-23 10:53:35 -0500

    Update taller02.py

--- talleres/taller02/taller02.py
+++ talleres/taller02/taller02.py
@@ -4,10 +4,14 @@ class Taller02:
       Taller02.mcd(y,x%y)
     else:
       return y
-    def comb(counter, lista, param):
-      if param != 0 and counter != len(lista):
-        return Taller02.comb(counter + 1, lista, param) or Taller02.comb(counter +1, lista, param - lista[counter])
-      elif param == 0:
-        return True
-      else:
-        return False
+  def comb(counter, lista, param):
+    if param != 0 and counter != len(lista):
+      return Taller02.comb(counter + 1, lista, param) or Taller02.comb(counter +1, lista, param - lista[counter])
+    elif param == 0:
+      return True
+    else:
+      return False
+  def comb2(pre,cadena):
+    if len(cadena)!=0:
+      Taller02.comb2(pre,cadena[1:len(cadena)])
+      Teller02.comb2(pre+cadena[0:1],cadena[1:len(cadena)])
