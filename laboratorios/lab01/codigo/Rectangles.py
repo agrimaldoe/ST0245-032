94668933b957db0f85148d6a8445c414a9fc8885
agrimaldoe <52968535+agrimaldoe@users.noreply.github.com>
2019-08-17 19:18:12 -0500

    Update Rectangles.py

--- laboratorios/lab01/codigo/Rectangles.py
+++ laboratorios/lab01/codigo/Rectangles.py
@@ -1,5 +1,5 @@
 def formas(n):
-  '''Función que calcula el número de formas se puede llenar un rectángulo de 2xn con rectángulos de 2x1'''
+  '''Function which calculate the number of ways for filling a rectangle of 2xn with rectangles of 2x1'''
   if(n <= 2):
     return n
   return formas(n-1) + formas(n-2)
