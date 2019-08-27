66b1a3454cff5b9576cceabbd0c159553398235c
asalazara1 <52968551+asalazara1@users.noreply.github.com>
2019-08-27 11:51:57 -0500

    Update Taller07.py

--- talleres/taller07/Taller07.py
+++ talleres/taller07/Taller07.py
@@ -9,11 +9,13 @@ class Node():
 class Lsimple():
     def __init__(self):
         self.first_Node = None
+        self.size=0
         
     def __void(self):
         return self.fisrt_Node == None
 
     def insert(self, element, index):
+        size=size+1
         if not self.head:
             self.head = node(data=data)
             return
@@ -34,6 +36,16 @@ class Lsimple():
     def size(self):
         return self.size
 
-    def remove(self):
+    def remove(self, element):
+        curr = self.head
+        prev = None
+        while curr and curr.data != element:
+            prev = curr
+            curr = curr.nxt
+        if prev is None:
+            self.head = curr.nxt
+        elif curr:
+            prev.nxt = curr.nxt
+            curr.nxt = None
    
     def contains(self):
