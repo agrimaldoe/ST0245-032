5840ecc7d67a4dc91c297692eff36af4d4258221
asalazara1 <52968551+asalazara1@users.noreply.github.com>
2019-08-30 11:02:58 -0500

    Update main.py

--- laboratorios/lab02/codigo/main.py
+++ laboratorios/lab02/codigo/main.py
@@ -38,12 +38,12 @@ def mergeSort(l):
 def insertSort(l): 
     for i in range(1, len(l)): 
   
-        key = l[i] 
+        actual = l[i] 
         j = i-1
-        while j >=0 and key < l[j] : 
+        while j >=0 and actual < l[j] : 
                 l[j+1] = l[j] 
                 j -= 1
-        l[j+1] = key 
+        l[j+1] = actual 
                
 def listaRand(n):
     lista = [0]  * n
