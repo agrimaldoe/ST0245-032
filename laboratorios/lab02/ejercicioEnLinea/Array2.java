5a0a2ab4ef30d2ed16d6165d97be12d2339cafd7
agrimaldoe <52968535+agrimaldoe@users.noreply.github.com>
2019-08-29 22:10:27 -0500

    Update Array2.java

--- laboratorios/lab02/ejercicioEnLinea/Array2.java
+++ laboratorios/lab02/ejercicioEnLinea/Array2.java
@@ -3,42 +3,42 @@ Array 2:
 1.
 public int countEvens(int[] nums) {
   int count = 0;
-  for(int i = 0; i < nums.length; i++){ //C1*n
-    if(nums[i] % 2 == 0) count++; //C2*n
+  for(int i = 0; i < nums.length; i++){ //C*n
+    if(nums[i] % 2 == 0) count++; //C*n
   }
-  return count; //C3
+  return count; //C
 }
 2.
 public boolean lucky13(int[] nums) {
-  for(int i = 0; i < nums.length; i++){ //C1*n
-    if(nums[i] == 1 || nums[i] == 3) //2*C2*n 
-      return false; //C3
+  for(int i = 0; i < nums.length; i++){ //C*n
+    if(nums[i] == 1 || nums[i] == 3) //2*C*n 
+      return false; //C
   }
-  return true; //C4
+  return true; //C
 }
 3.
 public boolean sum28(int[] nums) {
   int sum = 0;
-  for(int i = 0; i < nums.length; i++){ //C1*n
-    if(nums[i] == 2) sum+= 2; //C2*n
+  for(int i = 0; i < nums.length; i++){ //C*n
+    if(nums[i] == 2) sum+= 2; //C*n
   }
-  return sum == 8; //C3
+  return sum == 8; //C
 }
 4.
 public boolean more14(int[] nums) {
   int count1 = 0;
   int count4 = 0;
-  for(int i = 0; i < nums.length; i++){ //C1*n
-    if(nums[i] == 1) count1++; //C2*n
-    if(nums[i] == 4) count4++; //C3*n
+  for(int i = 0; i < nums.length; i++){ //C*n
+    if(nums[i] == 1) count1++; //C*n
+    if(nums[i] == 4) count4++; //C*n
   }
-  return count1 > count4; //C4
+  return count1 > count4; //C
 }
 5.
 public int[] fizzArray(int n) {
   int [] ans = new int [n]; 
-  for(int i = 0; i < n; i++){ //C1*n
+  for(int i = 0; i < n; i++){ //C*n
     ans[i] = i;
   }
-  return ans; //C2
+  return ans; //C
 }
