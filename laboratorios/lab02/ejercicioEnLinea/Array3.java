b86466dd89c3b7b497dde8af83f83311152325a9
agrimaldoe <52968535+agrimaldoe@users.noreply.github.com>
2019-08-29 22:36:11 -0500

    Update Array3.java

--- laboratorios/lab02/ejercicioEnLinea/Array3.java
+++ laboratorios/lab02/ejercicioEnLinea/Array3.java
@@ -2,83 +2,77 @@ Online Exercises:
 Array 3:
 1.
 public boolean linearIn(int[] outer, int[] inner) {
-  int count = 0;
-  for(int i = 0; i < inner.length; i++){ //C*n
-    for(int j = 0; j < outer.length; j++){ //C*n*n
-      if(inner[i] == outer[j]){ // C*n*n
-        count++;
-        break;
+  int count = 0; //C1
+  for(int i = 0; i < inner.length; i++){ //C2*n
+    for(int j = 0; j < outer.length; j++){ //C3*n*n
+      if(inner[i] == outer[j]){ //C4*n*n
+        count++; //C5
+        break; //C6
       }
     }
   }
-  return count == inner.length; //C
+  return count == inner.length; //C7
 }
 2.
 public int[] seriesUp(int n) {
-  int [] ans = new int [n*(n+1)/2];
-  int pos = 0;
-  for(int i = 1; i <= n; i++){ //C*n
-    for(int j = 1;j <= i; j++){ //C*n*n
-      ans[pos] = j;
-      pos++;
+  int [] ans = new int [n*(n+1)/2]; //C1
+  int pos = 0; //C2
+  for(int i = 1; i <= n; i++){ //C3*n
+    for(int j = 1;j <= i; j++){ //C4*n*n
+      ans[pos] = j; //C5*n*n
+      pos++; //C6*n*n
     }
   }
-  return ans; //C
+  return ans; //C7
 }
 3.
 public boolean canBalance(int[] nums) {
-  int acum1 = 0; 
-  int acum2 = 0;
-  for(int i = 0; i < nums.length; i++){ //C*n
-    acum1 += nums[i];
-    for(int j = i+1; j < nums.length; j++){//C*n*(n-1)
-      acum2 += nums[j];
+  int acum1 = 0; //C1
+  int acum2 = 0; //C2
+  for(int i = 0; i < nums.length; i++){ //C3*n
+    acum1 += nums[i]; //C4*n
+    for(int j = i+1; j < nums.length; j++){//C5*n*(n-1)
+      acum2 += nums[j]; //C6*n*(n-1)
     }
-    if(acum1 == acum2){ //C 
-      return true; //C
+    if(acum1 == acum2){ //C7*n 
+      return true; //C8
     }
-    acum2 = 0;
+    acum2 = 0; //C9*n
   }
-  return false; //C
+  return false; //C10
 }
 4.
 public int[] fix34(int[] nums) {
-  int aux = 0;
-  for(int i = 0; i < nums.length; i++){ //C*n
-    if(nums[i] == 3){ //C2*n
-      aux = nums[i+1];
-      nums[i+1] = 4; 
-      for(int j = i + 2; j < nums.length; j++){ //C*n*n
-        if(nums[j] == 4){ //C*n*n
-          nums[j] = aux;
+  int aux = 0; //C1
+  for(int i = 0; i < nums.length; i++){ //C2*n
+    if(nums[i] == 3){ //C3*n
+      aux = nums[i+1]; //C4
+      nums[i+1] = 4; //C5
+      for(int j = i + 2; j < nums.length; j++){ //C6*n*n
+        if(nums[j] == 4){ //C7*n*n
+          nums[j] = aux; //C8
         }
       }
     }
   }
-  return nums; //C
+  return nums; //C9
 }
 5.
 public int maxSpan(int[] nums) {
-  int maxSpan = 0;
-  for(int i = 0; i< nums.length; i++){ //C*n
-    int leftPos = 0;
-    int rightPos = 0;
-    for(int j = 0; j < nums.length; j++){ //C*n*n
-      if(nums[j] == nums[i]){ //C*n*n
-        leftPos=j;
-        break;
+  if(nums.length == 0){ //C1
+    return 0; //C2
+  } 
+    int maxSpan = 1; //C3
+    int span = 0 ; //C4
+    for(int i = 0; i < nums.length; i++){ //C5*n
+      for(int j = nums.length - 1; j >= 0; j--){ //C6*n*n
+        if(nums[j] == nums[i]){ //C7*n*n
+          span = j - i + 1; //C8
+           if(span > maxSpan){ //C9
+            maxSpan = span; //C10
+          }
+        }
       }
     }
-    for(int k = nums.length - 1; k >= 0; k--){ //C*n*n
-      if(nums[k] == nums [i]){ //C*n*n
-        rightPos= k;
-        break;
-      }
-    }
-      int span = rightPos - leftPos + 1;
-      if(span > maxSpan){ // C*n
-        maxSpan = span;
-      }
-  }
-  return maxSpan; //C
+  return maxSpan; //C11
 }
