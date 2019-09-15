ac2d2ef41a04c3566c4f188813ca279798030548
agrimaldoe <52968535+agrimaldoe@users.noreply.github.com>
2019-09-15 16:29:24 -0500

    Update Ejecicio2_1.java

--- laboratorios/lab03/ejercicioEnLinea/Ejecicio2_1.java
+++ laboratorios/lab03/ejercicioEnLinea/Ejecicio2_1.java
@@ -10,49 +10,49 @@ public class Ejecicio2_1
             String sub="";
             String a=sc.nextLine();
             LinkedList<String> lista= new LinkedList<String>();
-            for(int i=0; i<a.length();i++){ //O(n)
+            for(int i=0; i<a.length();i++){ //C1*n
                 if(a.charAt(i)=='['){ //O(n)
-                    sub = a.substring(aux,i); // O(n*n) 
-                    switch(code){
+                    sub = a.substring(aux,i); // C2*n*n 
+                    switch(code){ //O(n)
                         case -1:
-                            lista.addFirst(sub); //O(1)
+                            lista.addFirst(sub); //C3*n
                         break;
                         case 0:
-                            lista.addFirst(sub); //O(1)    
+                            lista.addFirst(sub); //C4*n    
                         break;
                         case 1:
-                            lista.addLast(sub);  //O(1) because LinkedList in java has a tail    
+                            lista.addLast(sub);  //C5*n because LinkedList in java has a tail    
                         break;
                     }
                     aux=i+1;
                     code=0;
-                }else if(a.charAt(i)==']'){ //O(n)
-                    sub = a.substring(aux,i); //O(n*n) 
+                }else if(a.charAt(i)==']'){ //C6*n
+                    sub = a.substring(aux,i); //C7*n 
                     switch(code){
                         case -1:
-                            lista.addFirst(sub); //O(1)
+                            lista.addFirst(sub); //C8*n
                         break;
                         case 0:
-                            lista.addFirst(sub);  //O(1)   
+                            lista.addFirst(sub);  //C9*n   
                         break;
                         case 1:
-                            lista.addLast(sub);   //O(1)   
+                            lista.addLast(sub);   //C10*n  
                         break;
                     }
                     aux=i+1;
                     code=1;
                 }
             }
-            sub = a.substring(aux); //O(n)
-            switch(code){
+            sub = a.substring(aux); //C11*n
+            switch(code){ // C12
                 case -1:
-                    lista.addFirst(sub); //O(1)
+                    lista.addFirst(sub); 
                 break;
                 case 0:
-                    lista.addFirst(sub);  //O(1) 
+                    lista.addFirst(sub);   
                 break;
                 case 1:
-                    lista.addLast(sub);  //O(1)    
+                    lista.addLast(sub);     
                 break;
             }
             printlista(lista);
