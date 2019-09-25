8a3694c6f3ed5d02d28a4e07d61465a4f000a509
agrimaldoe <52968535+agrimaldoe@users.noreply.github.com>
2019-09-25 15:19:52 -0500

    Add files via upload

---
+++ talleres/taller10/main.py
@@ -0,0 +1,70 @@
+class Nodo:
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.data = data
+
+    def __repr__(self):
+        return f'{self.data}'
+
+class BinaryTree:
+  def __init__(self):
+      self.root = None
+    
+  def insertar(self, data):
+    if self.root is None: 
+      self.root = Nodo(data)
+    else:
+      self.__insertar_aux(data, self.root)
+
+  def __insertar_aux(self, data, actual):
+      if(actual.right is None or actual.left is None):
+        if(data > actual.data):
+          actual.right = Nodo(data)
+        else:
+          actual.left = Nodo(data)
+       
+      else:
+        if(data > actual.data):
+            self.__insertar_aux(data, actual.right)
+        else:
+            self.__insertar_aux(data, actual.left)
+      return actual
+       
+
+    # ------------------------------------------------------    
+  def buscar(self, data):
+      return self.__buscar_aux(data, self.root)
+
+  def __buscar_aux(self, data, actual):
+      if(actual is None):
+          return False
+      else:
+          if(data == actual.data):
+              return True
+          else:
+              if(data > actual.data):
+                  return self.__buscar_aux(data, actual.right)
+              else:
+                  return self.__buscar_aux(data, actual.left)
+      
+     
+  def imprimir(self):
+      self.__imprimir_aux(self.root)
+        
+  def __imprimir_aux(self, actual):
+      if actual is not None:
+          self.__imprimir_aux(actual.left)
+          print(actual.data)
+          self.__imprimir_aux(actual.right)
+  
+       
+ 
+
+
+b = BinaryTree()
+b.insertar(4)
+b.insertar(3)
+b.insertar(5)
+print(b.buscar(3))
+b.imprimir()
