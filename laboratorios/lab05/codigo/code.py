2c3be0924084d4eb67607e52a9b90904cad6137a
asalazara1 <52968551+asalazara1@users.noreply.github.com>
2019-10-21 19:43:34 -0500

    Update code.py

--- laboratorios/lab05/codigo/code.py
+++ laboratorios/lab05/codigo/code.py
@@ -1,17 +1,29 @@
 import networkx as nx
 
-f = open("doc2.txt")
+f = open("doc.txt")
 f1=f.readlines()
 f2=[]
 for x in f1:
     f2.append(x.split(" "))
-    
-Gr=nx.DiGraph()
+
+Lista = {}
+
 contador=0
+f2.pop(0)
+while not f2[0][0] == "Arcos.":
+    try: 
+        x = f2.pop(0)
+        Lista[x[0]]=[contador,x[1],x[2]]
+        contador = contador + 1
+    
+    except:
+        continue
+        
+f2.pop(0)
+Gr=nx.DiGraph()
 for x in f2:
-    x[0]=contador
     
     Gr.add_edge(
-        x[0],x[1],peso=x[2]
+        Lista[x[0]][0],Lista[x[1]][0],peso=x[2]
     )
 Gr.nodes(data=True)
